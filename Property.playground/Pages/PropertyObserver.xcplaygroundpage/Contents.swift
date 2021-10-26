e148b340398661df22865c01cdccd212dc9d35b3
Beepeach <eh7457303j@gmail.com>
2021-10-26 11:20:33 +0900

    Property Observer 예제 변경

--- Property.playground/Pages/PropertyObserver.xcplaygroundpage/Contents.swift
+++ Property.playground/Pages/PropertyObserver.xcplaygroundpage/Contents.swift
@@ -6,19 +6,38 @@ import Foundation
 // var로 선언된 stored property에서 선언 가능하다.
 // 예외로 subclass에 override한 computed Property에서 사용가능하다.
 
-/*
-var name: Type = DefaultValue {
-    willSet(name) {
-        statement
-    }
-    didSet(name) {
-        statment
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("Set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
     }
 }
-*/
 
-class Size {
-    var width = 0.0 {
+let stepCounter: StepCounter = StepCounter()
+
+stepCounter.totalSteps = 200
+// Set totalSteps to 200
+// Added 200 steps
+
+stepCounter.totalSteps = 600
+// Set totalSteps to 600
+// Added 400 steps
+
+stepCounter.totalSteps = 600
+// Set totalSteps to 600
+
+
+// MARK: - NonDefault
+
+
+struct Size {
+    var width: Int {
         willSet {
             print("\(width) will change \(newValue)")
         }
@@ -28,8 +47,13 @@ class Size {
     }
 }
 
-let s: Size = Size()
-s.width = 100
+var square: Size = Size(width: 10)
 
+square.width = 20
+// 10 will change 20
+// 10 is changed by 20
 
+square.width = 20
+//20 will change 20
+//20 is changed by 20
 //: [Next](@next)
