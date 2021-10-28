6ed4255f6924941a7d4a3861001295d76bc2d915
Beepeach <eh7457303j@gmail.com>
2021-10-28 10:47:52 +0900

    jSON 테스트 연습

---
+++ JSONTest.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+struct GroceryProduct: Codable {
+    var name: String
+    var points: Int
+    var description: String?
+}
+
+let pear = GroceryProduct(name: "Pear", points: 250, description: "A ripe pear")
+
+let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+
+do {
+    let data = try encoder.encode(pear)
+    print(String(data: data, encoding: .utf8))
+} catch {
+    print(error)
+}
+
+let json =
+"""
+{
+    "name": "Durian",
+    "points": 600,
+    "description": "A fruit with a distinctive scent"
+}
+""".data(using: .utf8)!
+
+let decoder: JSONDecoder = JSONDecoder()
+
+do {
+    let product = try decoder.decode(GroceryProduct.self, from: json)
+    print(product)
+} catch {
+    print(error)
+}
+
