9eaa99ff3909f046c9c0f1bee3dded11879e49c4
Beepeach <eh7457303j@gmail.com>
2021-11-04 16:47:38 +0900

    UserDefaults에 저장되는지 확인

--- PropertyWrapper.playground/Pages/DefaultsTest.xcplaygroundpage/Contents.swift
+++ PropertyWrapper.playground/Pages/DefaultsTest.xcplaygroundpage/Contents.swift
@@ -2,6 +2,58 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+@propertyWrapper
+struct UserDefaultsHelper<Value> {
+    let key: String
+    let value: Value
+    
+    init(key: String, value: Value) {
+        self.key = key
+        self.value = value
+        
+        UserDefaults.standard.set(self.value, forKey: self.key)
+    }
+    
+    public var wrappedValue: Value {
+        get {
+            return UserDefaults.standard.object(forKey: self.key) as? Value ?? self.value
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: self.key)
+            print("Save")
+        }
+    }
+    
+    func remove() {
+        UserDefaults.standard.removeObject(forKey: key)
+        print("Remove")
+    }
+    
+    func reset() {
+        UserDefaults.standard.set(self.value, forKey: self.key)
+    }
+}
+
+struct MySoundSetting {
+    @UserDefaultsHelper(key: "myVolume", value: 60)
+    var volume: Int
+    
+    @UserDefaultsHelper(key: "myStereo", value: false)
+    var isStereo: Bool
+    
+    func removeAll() {
+        _volume.remove()
+        _isStereo.remove()
+    }
+    
+    func reset() {
+        _volume.reset()
+        _isStereo.reset()
+    }
+}
+
+var test: MySoundSetting = MySoundSetting()
+UserDefaults.standard.object(forKey: "myVolume") as? Int
+
 
 //: [Next](@next)
