6472e4d05ccc4dc9c6a37bf8186b48ca73110451
Beepeach <eh7457303j@gmail.com>
2021-12-08 00:56:29 +0900

    진수를 변환하는 방법에 대해서 공부

--- ChangeNumber.playground/Contents.swift
+++ ChangeNumber.playground/Contents.swift
@@ -1,6 +1,15 @@
 import UIKit
 
+// 10진수 -> 다른 진수는 Int -> String으로
 let decimal: Int = 20
 
-let binary: String = String(decimal, radix: 2, uppercase: true)
-binary
+let binary: String = String(decimal, radix: 2)
+print(binary)
+
+// 다른진수 -> 10진수는 String -> Int로
+let hexaDecimal: String = String(487, radix: 16, uppercase: true)
+print(hexaDecimal)
+
+let hexaToDecimal: Int = Int(hexaDecimal, radix: 16) ?? 0
+print(hexaToDecimal)
+
