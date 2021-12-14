c00179b57e86ded23924caa297edf069b2cc0f02
Beepeach <eh7457303j@gmail.com>
2021-12-14 15:19:57 +0900

    Init Inheritance 예제 추가

--- Initializer.playground/Pages/InheritanceAndOverride.xcplaygroundpage/Contents.swift
+++ Initializer.playground/Pages/InheritanceAndOverride.xcplaygroundpage/Contents.swift
@@ -30,5 +30,59 @@ class Rectangle: Figure {
 }
 
 
+// MARK: - Inheritance Exam
+
+class Food {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    convenience init() {
+        self.init(name: "[Unnamed]")
+    }
+}
+
+class RecipeIngredient: Food {
+    var quantity: Int
+    
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
+    }
+}
+
+let sixEggs = RecipeIngredient(name: "Egg", quantity: 6)
+
+let bacon = RecipeIngredient(name: "Bacon")
+
+
+class ShoppingListItem: RecipeIngredient {
+    var purchased: Bool = false
+    var description: String {
+        var output = "\(quantity) X \(name)"
+        output += purchased ? " ✔" : " ✘"
+        
+        return output
+    }
+}
+
+var breakfastList = [
+    ShoppingListItem(),
+    ShoppingListItem(name: "Bacon"),
+    ShoppingListItem(name: "Eggs", quantity: 6)
+]
+
+breakfastList[0].name = "Orange juice"
+breakfastList[0].purchased = true
+
+for breakfast in breakfastList {
+    print(breakfast.description)
+}
 
 //: [Next](@next)
