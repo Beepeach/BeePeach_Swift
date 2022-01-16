9e69dce496cbb249021edb3e10c3d236f1e62224
Beepeach <eh7457303j@gmail.com>
2022-01-16 12:40:20 +0900

    Feat: Comparable 프로토콜 예제 변경 및 복습

--- BasicProtocol.playground/Pages/Comparable.xcplaygroundpage/Contents.swift
+++ BasicProtocol.playground/Pages/Comparable.xcplaygroundpage/Contents.swift
@@ -3,43 +3,67 @@
 import Foundation
 
 // MARK: - Comparable
-// > < >= <=와 같이 대소를 비교하려면 꼭 채용해야한다.
-// 기본적으로 String, Number들은 채용하고 있다.
-// Equatable을 채용하고있다.
+// >, < 비교연산자를이용해 대소를 비교하려면 꼭 채용해야한다.
+// Equatable을 상속하고 있다.
 // 4가지 요구사항중 < 만 구현해도 된다.
 
-// MARK: - Enum
-// 연관값이 없어도 자동으로 제공하지않고 채용만 하면 알아서 구현해준다.
-// rawValue를 사용한다면 직접 구현해줘야한다.
 
-enum Weekday: Int {
-    case sunday
-    case monday
-    case tuesday
-    case wednesday
-    case thursday
-    case friday
-    case saturday
-}
+// MARK: - Class
 
-extension Weekday: Comparable {
-    static func < (lhs: Weekday, rhs: Weekday) -> Bool {
-        return lhs.rawValue < rhs.rawValue
+class CustomDate {
+    let year: Int
+    let month: Int
+    let day: Int
+    
+    init(year:Int, month: Int, day: Int) {
+        self.year = year
+        self.month = month
+        self.day = day
     }
 }
 
-Weekday.sunday > Weekday.monday
-Weekday.saturday <= Weekday.thursday
+extension CustomDate: Comparable {
+    static func < (lhs: CustomDate, rhs: CustomDate) -> Bool {
+        if lhs.year != rhs.year {
+            return lhs.year < rhs.year
+        } else if lhs.month != rhs.month {
+            return lhs.month < rhs.month
+        } else {
+            return lhs.day < rhs.day
+        }
+    }
 
-// 연관값이 있고 타입이 모두 Comparable을 채용하고 있다면 채용만 해주면 된다.
+    static func == (lhs: CustomDate, rhs: CustomDate) -> Bool {
+        return (
+            lhs.year == rhs.year &&
+            lhs.month == rhs.month &&
+            lhs.day == rhs.day
+        )
+    }
+}
 
+
+let goodDay = CustomDate(year: 2021, month: 12, day: 24)
+let birthDay = CustomDate(year: 2022, month: 1, day: 15)
+
+goodDay >= birthDay
+
+
+let date1 = CustomDate(year: 2020, month: 10, day: 11)
+let date2 = CustomDate(year: 2021, month: 6, day: 8)
+let date3 = CustomDate(year: 2021, month: 11, day: 26)
+let date4 = CustomDate(year: 1990, month: 9, day: 21)
+
+var dates: [CustomDate] = [date1, date2, date3, date4]
+
+dates.sort()
 
 
 // MARK: - Struct
 // 알아서 제공안해준다!
-// 무엇을 기준으로 비교할건지 목적에 맞게 우리가 정해야한다.
+// 하지만 Equtable은 제공해주기때문에 모든 저장프로퍼티가 Equtable을 채용한 형식이라면 == 구현을 하지 않아도 된다.
 
-struct Person: Equatable {
+struct Person {
     let name: String
     let age: Int
 }
@@ -50,16 +74,66 @@ extension Person: Comparable {
     }
 }
 
-let older: Person = Person(name: "Beepeach", age: 100)
-let younger: Person = Person(name: "Somsom", age: 10)
+let beepeach: Person = Person(name: "Beepeach", age: 26)
+let judy: Person = Person(name: "Judy", age: 22)
 
-older > younger
+beepeach > judy
 
 
 
-// MARK: - Class
-// 당연히 제공 안해준다.
+// MARK: - Enum
+// 연관값이 없어도 자동으로 제공하지않고 채용만 하면 알아서 구현해준다.
+// rawValue를 사용한다면 직접 구현해줘야한다.
+// 연관값이 있고 타입이 모두 Comparable을 채용하고 있다면 채용만 해주면 된다.
+// 설명엔 꼭 original에 채용하라고 나오는데 extension에서 채용해도 문제가 없다?!?
 
+//enum School: Comparable {
+//    case elementary
+//    case middle
+//    case high
+//    case university
+//}
+//
+//School.elementary < School.middle
+//School.university > School.high
+
+
+//enum School {
+//    case elementary(grade: Int)
+//    case middle(grade: Int)
+//    case high(grade: Int)
+//    case university(grade: String, department: String)
+//}
+//
+//extension School: Comparable {
+//
+//}
+//
+//School.elementary(grade: 2) > School.middle(grade: 3)
+//School.elementary(grade: 4) < School.elementary(grade: 5)
+//School.elementary(grade: 6) > School.high(grade: 1)
+//
+//School.university(grade: "freshman", department: "Computer Science") > School.high(grade: 1)
+//School.university(grade: "freshman", department: "Math") > School.university(grade: "freshman", department: "Computer Science")
+
+
+enum School: Int {
+    case elementary
+    case middle
+    case high
+    case university
+}
+
+extension School: Comparable {
+    static func < (lhs: School, rhs: School) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+}
+
+School.high > School.middle
+// true
+School(rawValue: 3)! < School(rawValue: 0)!
+// false
 
 
 
