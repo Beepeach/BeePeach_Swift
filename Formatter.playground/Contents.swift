8b993de2c38b8f7da97a2ccbba7bf0d179071e1c
Beepeach <eh7457303j@gmail.com>
2022-01-23 00:58:41 +0900

    Refact: 잘못된 예시를 변경

--- Formatter.playground/Contents.swift
+++ Formatter.playground/Contents.swift
@@ -1,8 +1,20 @@
 import UIKit
 
-let dateStr = "1970-01-01T00:00:00" + "Z"
-print(dateStr)
+// "Z"는 시간대를 의미 UTC라면 Z 나머지 시간대는 +09:00과 같이 표시한다.
+var dateStr = "1970-01-01T00:00:00" + "Z"
 let iso8601Formatter = ISO8601DateFormatter()
 //iso8601Formatter.formatOptions = [.withFullTime, .withFullDate, .withDashSeparatorInDate, .withColonSeparatorInTime]
 
-iso8601Formatter.date(from: dateStr)
+var date = iso8601Formatter.date(from: dateStr)
+print(date)
+
+
+dateStr = "1970-01-01T00:00:00"
+// withTime으로 하면 nil이 안되고 잘 출력이 된다.
+
+// withFullTime으로 하면 "Z"를 추가해야한다.
+// 그 이유는?!?!
+iso8601Formatter.formatOptions = [.withTime, .withFullDate, .withDashSeparatorInDate, .withColonSeparatorInTime]
+
+date = iso8601Formatter.date(from: dateStr)
+print(date)
