c3d327241f411d38d84765a37be48751d8e9723a
Beepeach <eh7457303j@gmail.com>
2022-03-24 20:25:04 +0900

    Refact: Error에 관한 예제 변경

--- Swift/Error.playground/Pages/Error.xcplaygroundpage/Contents.swift
+++ Swift/Error.playground/Pages/Error.xcplaygroundpage/Contents.swift
@@ -1,62 +1,70 @@
 import UIKit
 
 // MARK: - ERROR
-// Compile Time Error
-// 대부분 문법과 관련되어있다.
-// 컴파일러가 잘 잡아준다.
-
 // Run Time Error
 // 프로그램이 실행하는 도중 발생한다.
 
 
 // MARK: - Error Type 만들기
-// Error 프로토콜을 채용한 Type, 대부분 enum으로 만들어둔다.
-// Swift Error처리 시스템에 통합된다.
+// Error 프로토콜을 채용한 Type, enum으로 만들면 편합니다.
+// Error 프로토콜은 Type을 에러가 에러처리에사용될수 있음을 나타냅니다.
 
-enum DataParsingError: Error {
-    case invalidType
-    case invalidField
-    case missingRequiredField(String)
+enum NumberError: Error {
+    case largeNumber(over: Int)
+    case negativeNumber
 }
 
 
 // MARK: - Throw
-// 에러를 발생시키는것을 에러를 throw한다고 표현한다.
-// 제한없이 에러를 발생시킬수 없다.
+// 에러를 발생시키는것을 에러를 throw 한다고 표현합니다.
 // 에러가 발생할 수 있다고 선언이 필요하다.
 // 함수, 생성자, 클로저가 에러를 발생시킬수 있다.
 // throws를 통해서 에러를 던질수 있다고 표현할 수 있다.
 
-// throw expression(Error 인스턴스)
+// throw 키워드를 이용해서 에러를 throw할 수 있습니다.
+throw NumberError.largeNumber(over: 1000)
+
+
+// MARK: - Throws
 // func name(parameter) throws -> ReturnType { }
 // init(parameters) throws { }
 // { (parameter) throws -> ReturnType in }
 
-// throw를 아무곳에서 사용할 수 없는 예제
+
+// throw를 아무곳에서 사용할 수 없습니다.
 /*
-func parsing(data: [String: Any]) {
-    guard let _ = data["name"] else {
-        throw DataParsingError.missingRequiredField("name")
+func validate(num: Int) {
+    guard num > 1000 else {
+        throw NumberError.largeNumber(over: 1000)
     }
+    
+    print("This num is less than 1000")
 }
 */
 
-func parsing(data: [String: Any]) throws {
-    guard let _ = data["name"] else {
-        throw DataParsingError.missingRequiredField("name")
+func validate(num: Int) throws {
+    guard num > 1000 else {
+        throw NumberError.largeNumber(over: 1000)
     }
     
-    guard let _ = data["age"] as? Int else {
-        throw DataParsingError.invalidType
+    guard num > 0 else {
+        throw NumberError.negativeNumber
     }
     
-    print("Success!!")
+    print("This positive num is less than 1000")
 }
+
 
 
 
 // MARK: - throw하는 함수를 호출하는 방법
 // 기본 함수처럼 그냥 호출할 수 없다.
+
+// ERROR!!
+// validate(num: 100)
+
+
+
 // try, try?, try!와 함께 사용해야한다.
 // try는 do catch와 함께 사용해야한다.
 // try?는 에러가 발생하면 nil을 리턴하게된다.
@@ -64,15 +72,10 @@ func parsing(data: [String: Any]) throws {
 // 그냥 호출하면 에러가 나는 예제
 // parsing(data: ["name": "Beepeach"])
 
-// 실패
-try? parsing(data: [:])
-try? parsing(data: ["name": "Beepeach"])
 
-// 성공
-try? parsing(data: ["name": "Beepeach", "age": 30])
 
 
 // MARK: - throw 함수를 처리하는 세가지 방법
 // 1. do - catch
-// 2. try? - optionalBiding
+// 2. try? - optional Biding
 // 3. rethrow
