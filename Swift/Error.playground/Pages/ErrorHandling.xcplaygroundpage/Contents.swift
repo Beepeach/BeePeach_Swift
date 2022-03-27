9ac227f48cefef28c13b2abdd75dd534a9214b67
Beepeach <eh7457303j@gmail.com>
2022-03-27 14:33:35 +0900

    Refact: Rethrow 예제 추가
    사용방법에 있어서 추가사항이있어서 더 추가했습니다.

--- Swift/Error.playground/Pages/ErrorHandling.xcplaygroundpage/Contents.swift
+++ Swift/Error.playground/Pages/ErrorHandling.xcplaygroundpage/Contents.swift
@@ -81,31 +81,92 @@ enum MyError: Error {
     case rethrowsError
 }
 
-
-func throwsFunction() throws {
+func someErrorFunction() throws {
     throw MyError.throwsError
+}
+
+
+func nonThrowsFunction(callback: () throws -> Void) {
+    // ERROR: throws가 아니라면 그냥 try하면 에러가 발생합니다.
+    // try callback()
+    
+    // throws 키워드없이 호출하려면 do-catch 또는 try?로 에러 처리를 해줘야합니다.
+    do {
+        try callback()
+    } catch {
+        print(error)
+    }
+}
+
+func throwsFunction(callback: () throws -> Void) throws {
+    try callback()
+    
+    // throws는 내부에서 바로 throw를 할 수 있습니다. (Propagate Error)
+    // throw MyError.throwsError
 }
 
 
 func rethrowsFunction(callback: () throws -> Void) rethrows {
     try callback()
+    
+    // rethrows는 내부에서 바로 throws 할 수 없습니다.
+    // ERROR!!
+    // throw MyError.rethrowsError
 }
 
-func anotherRethrowsFunction(callback: () throws -> Void) rethrows {
+func anotherThrowsFunction(callback: () throws -> Void) throws {
+    // throws는 외부함수에 대한 제약도 없습니다.
     do {
         try callback()
     } catch {
         throw MyError.rethrowsError
     }
     
+    do {
+        try someErrorFunction()
+    } catch {
+        throw MyError.rethrowsError
+    }
+}
+
+func anotherRethrowsFunction(callback: () throws -> Void) rethrows {
+    do {
+        try callback()
+    } catch {
+        // catch에서는 throw할 수 있습니다.
+        throw MyError.rethrowsError
+    }
+    
     // 파라미터로 전달한 함수가 아니라면 throw할 수 없습니다.
     do {
-        try throwsFunction()
+        try someErrorFunction()
     } catch {
-        // Error
+        // ERROR!!
         // throw MyError.rethrowsError
     }
 }
+
+
+// throws와 rethrows의 차이
+// map, filter등이 rethrows에 해당합니다.
+// rethrows는 파라미터로 throws를 받는데 중요한건 반드시 오는 함수가 throws라는 말은 아닙니다.
+// 그냥 일반 함수가 올 수도 있습니다.
+// 즉 rethrows는 꼭 try를 붙이지 않아도됩니다. 파라미터로 전달된 함수가 throws일때만 try로 호출합니다.
+func generalFunc() {
+    print("No throws")
+}
+
+func throwsFunc() throws {
+    MyError.throwsError
+    print("Throw Error")
+}
+
+try throwsFunction(callback: generalFunc)
+try throwsFunction(callback: throwsFunc)
+
+rethrowsFunction(callback: generalFunc)
+try rethrowsFunction(callback: throwsFunc)
+
 
 // MARK: - 2. Do Catch Statement
 // do 는 필수 catch는 선택
